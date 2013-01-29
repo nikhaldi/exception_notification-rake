972133c457c4d9fa50da941e7362df41a186b76e
Nik Haldimann <nhaldimann@gmail.com>
2013-01-29 17:45:14 -0500

    Some configuration basics

--- lib/exception_notifier/rake/rake.rb
+++ lib/exception_notifier/rake/rake.rb
@@ -3,8 +3,33 @@ require 'exception_notifier'
 class ExceptionNotifier
   class Rake
 
+    @notifier_options = {}
+
     def self.configure(config, options = {})
-      # TODO
+      # TODO add ExceptionNotifier to middleware if needed
+      @notifier_options.merge!(default_notifier_options)
+      @notifier_options.merge!(options)
+    end
+
+    def self.default_notifier_options
+      {
+        :email_prefix => "[Rake Failure] ",
+        # TODO add stdin/stderr sections with captured output
+        :background_sections => %w(backtrace),
+      }
+    end
+
+    def self.notifier_options
+      @notifier_options
+    end
+
+    def self.deliver_notification(exception)
+      ExceptionNotifier::Notifier.background_exception_notification(
+        exception, notifier_options).deliver
+    end
+
+    def self.reset_for_test
+      @notifier_options = {}
     end
   end
 end
--- test/rake_test.rb
+++ test/rake_test.rb
@@ -1,8 +1,25 @@
 require 'test/unit'
+require 'exception_notifier/rake'
 
 class RakeTest < Test::Unit::TestCase
 
-  def test_foo
-    assert true
+  def setup
+    ExceptionNotifier::Rake.reset_for_test
+  end
+
+  def test_configure_only_default_options
+    ExceptionNotifier::Rake.configure({})
+    assert_equal ExceptionNotifier::Rake.default_notifier_options,
+      ExceptionNotifier::Rake.notifier_options
+  end
+
+  def test_configure_custom_options
+    some_options = {
+      :sender_address => 'foo@example.com',
+      :exception_recipients => ['bar@example.com'],
+    }
+    ExceptionNotifier::Rake.configure({}, some_options)
+    assert_equal some_options.merge(ExceptionNotifier::Rake.default_notifier_options),
+      ExceptionNotifier::Rake.notifier_options
   end
 end
