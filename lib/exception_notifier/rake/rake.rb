ba926f34b65b8708ad054ea983ba3b4ef333cd80
Nik Haldimann <nhaldimann@gmail.com>
2013-02-01 17:31:23 +0100

    Reverted experiment with trying to manipulate middleware stack in-flight

--- lib/exception_notifier/rake/rake.rb
+++ lib/exception_notifier/rake/rake.rb
@@ -3,30 +3,15 @@ require 'exception_notifier'
 class ExceptionNotifier
   class Rake
 
-    ALWAYS_TRUE = lambda { true }
-
     @notifier_options = {}
 
     def self.configured?
       !@notifier_options.empty?
     end
 
-    def self.configure(config, options = {})
+    def self.configure(options = {})
       @notifier_options.merge!(default_notifier_options)
       @notifier_options.merge!(options)
-
-      if notifier_middleware_configured?(config)
-        # We're adding a catch-all ignore_if rule by default for cases where
-        # no explicit ExceptionNotifier was installed.
-        @notifier_options.delete :ignore_if
-      else
-        config.middleware.use ExceptionNotifier
-      end
-    end
-
-    def self.notifier_middleware_configured?(config)
-      # TODO this probably won't work in reality ...
-      !!config.middleware.detect {|ware| ware.klass == ExceptionNotifier}
     end
 
     def self.default_notifier_options
@@ -34,7 +19,6 @@ class ExceptionNotifier
         :email_prefix => "[Rake Failure] ",
         # TODO add stdin/stderr sections with captured output
         :background_sections => %w(backtrace),
-        :ignore_if => ALWAYS_TRUE,
       }
     end
 
--- test/rake_test.rb
+++ test/rake_test.rb
@@ -1,4 +1,3 @@
-require 'rails/application/configuration'
 require 'test/unit'
 
 require 'exception_notifier/rake'
@@ -8,20 +7,14 @@ class RakeTest < Test::Unit::TestCase
   def setup
     ExceptionNotifier::Rake.reset_for_test
     assert !ExceptionNotifier::Rake.configured?
-    @mock_config = Rails::Application::Configuration.new
-    @mock_config.middleware = ActionDispatch::MiddlewareStack.new
   end
 
-  def assert_has_notifier_middleware
-    assert ExceptionNotifier::Rake.notifier_middleware_configured? @mock_config
-  end
 
   def test_configure_only_default_options
-    ExceptionNotifier::Rake.configure(@mock_config)
+    ExceptionNotifier::Rake.configure
     assert ExceptionNotifier::Rake.configured?
     assert_equal ExceptionNotifier::Rake.default_notifier_options,
       ExceptionNotifier::Rake.notifier_options
-    assert_has_notifier_middleware
   end
 
   def test_configure_custom_options
@@ -29,25 +22,9 @@ class RakeTest < Test::Unit::TestCase
       :sender_address => 'foo@example.com',
       :exception_recipients => ['bar@example.com'],
     }
-    ExceptionNotifier::Rake.configure(@mock_config, some_options)
+    ExceptionNotifier::Rake.configure some_options
     assert ExceptionNotifier::Rake.configured?
     assert_equal some_options.merge(ExceptionNotifier::Rake.default_notifier_options),
       ExceptionNotifier::Rake.notifier_options
-    assert_has_notifier_middleware
-  end
-
-  def test_configure_with_middleware_installed
-    @mock_config.middleware.use ExceptionNotifier
-    assert_has_notifier_middleware
-    ExceptionNotifier::Rake.configure(@mock_config)
-    assert ExceptionNotifier::Rake.configured?
-    assert_has_notifier_middleware
-  end
-
-  def test_configure_with_middleware_installed_skips_ignore_if
-    @mock_config.middleware.use ExceptionNotifier
-    ExceptionNotifier::Rake.configure(@mock_config)
-    assert ExceptionNotifier::Rake.configured?
-    assert !ExceptionNotifier::Rake.notifier_options[:ignore_if]
   end
 end
